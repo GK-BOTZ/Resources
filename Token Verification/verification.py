acabd443445729acd7006a2faa8daf066684f2dc
GK BOTZ <131459110+GK-BOTZ@users.noreply.github.com>
2025-02-01 22:54:29 +0530

    Update verification.py

--- Token Verification/verification.py
+++ Token Verification/verification.py
@@ -8,27 +8,25 @@ from cloudscraper import create_scraper
 from motor.motor_asyncio import AsyncIOMotorClient
 
 
-
 # Config Variables 😄
-BOT_TOKEN = os.environ.get('BOT_TOKEN', '')   # YOUR BOT TOKEN FROM @Botfather
-VERIFY_PHOTO = os.environ.get('VERIFY_PHOTO', '')  # YOUR VERIFY PHOTO LINK
-SHORTLINK_URL = os.environ.get('SHORTLINK_URL', '') # YOUR SHORTLINK URL LIKE:- site.com
-SHORTLINK_API = os.environ.get('SHORTLINK_API', '') # YOUR SHORTLINK API LIKE:- ma82owowjd9hw6_js7
-VERIFY_EXPIRE = os.environ.get('VERIFY_EXPIRE', '') # VERIFY EXPIRE TIME IN SECONDS. LIKE:- 0 (ZERO) TO OFF VERIFICATION 
-VERIFY_TUTORIAL = os.environ.get('VERIFY_TUTORIAL', '') # LINK OF TUTORIAL TO VERIFY 
-DATABASE_URI = os.environ.get('DATABASE_URI', '') # MONGODB DATABASE URL
+VERIFY_PHOTO = os.environ.get('VERIFY_PHOTO', 'https://envs.sh/s/7ddExUc0Ii8rYftCLx_cww/SnT.png')  # YOUR VERIFY PHOTO LINK
+SHORTLINK_SITE = os.environ.get('SHORTLINK_SITE', 'runurl.in') # YOUR SHORTLINK URL LIKE:- site.com
+SHORTLINK_API = os.environ.get('SHORTLINK_API', '1277706b49fbfbe00746b85d5e12f5b3e4bae1fd') # YOUR SHORTLINK API LIKE:- ma82owowjd9hw6_js7
+VERIFY_EXPIRE = os.environ.get('VERIFY_EXPIRE', 3600) # VERIFY EXPIRE TIME IN SECONDS. LIKE:- 0 (ZERO) TO OFF VERIFICATION 
+VERIFY_TUTORIAL = os.environ.get('VERIFY_TUTORIAL', 'https://t.me/pundiliyaa/8') # LINK OF TUTORIAL TO VERIFY 
+DATABASE_URL = os.environ.get('DATABASE_URL', '') # MONGODB DATABASE URL To Store Verifications 
+COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'post_finder')   # Collection Name For MongoDB 
 
-
-bot_id = BOT_TOKEN.split(':')[0]
 verify_dict = {}
-missing=[v for v in ["BOT_TOKEN", "VERIFY_PHOTO", "SHORTLINK_URL", "SHORTLINK_API", "VERIFY_EXPIRE", "VERIFY_TUTORIAL"] if not os.getenv(v)]; sys.exit(f"Missing: {', '.join(missing)}") if missing else None
+missing=[v for v in ["COLLECTION_NAME", "VERIFY_PHOTO", "SHORTLINK_SITE", "SHORTLINK_API", "VERIFY_TUTORIAL"] if not v]; sys.exit(f"Missing: {', '.join(missing)}") if missing else None
+
 
 # Databse Code
 class VerifyDB():
     def __init__(self):
-        self._dbclient = AsyncIOMotorClient(DATABASE_URI)
+        self._dbclient = AsyncIOMotorClient(DATABASE_URL)
         self._db = self._dbclient['verify-db']
-        self._verifydb = self._db[bot_id]  
+        self._verifydb = self._db[COLLECTION_NAME]  
 
     async def get_verify_status(self, user_id):
         if status := await self._verifydb.find_one({'id': user_id}):
@@ -40,37 +38,39 @@ class VerifyDB():
 
 verifydb = VerifyDB()
 
-# Helper Functions
+# FUNCTIONS
 async def is_user_verified(user_id):
     if not VERIFY_EXPIRE:
         return True
     isveri = await verifydb.get_verify_status(user_id)
-    if not isveri or (time() - isveri) >= float(Config.VERIFY_EXPIRE):
+    if not isveri or (time() - isveri) >= float(VERIFY_EXPIRE):
         return False
     return True    
     
-async def send_verification(client, message, text=None):
-    username = await client.get_me().username
-    verify_token = await get_verify_token(client, message.from_user.id, f"https://telegram.me/{username}?start=")
-    buttons = InlineKeyboardMarkup([
-        [InlineKeyboardButton('Get Token', url=verify_token)],
-        [InlineKeyboardButton('🎬 Tutorial 🎬', url=VERIFY_TUTORIAL)],
-    ]),
+async def send_verification(client, message, text=None, buttons=None):
+    username = (await client.get_me()).username
+    if done := await is_user_verified(message.from_user.id):
+        text = f'<b>Hi 👋 {message.from_user.mention},\nYou Are Already Verified Enjoy 😄</b>'
+    else:
+        verify_token = await get_verify_token(client, message.from_user.id, f"https://telegram.me/{username}?start=")
+        buttons = InlineKeyboardMarkup([
+            [InlineKeyboardButton('Get Token', url=verify_token)],
+            [InlineKeyboardButton('🎬 Tutorial 🎬', url=VERIFY_TUTORIAL)]
+        ])
     if not text:
         text = f"""<b>Hi 👋 {message.from_user.mention}, 
-Your Ads Token Has Been Expired, Kindly Get A New Token To Continue Using This Bot.
+<blockquote expandable>\nYour Ads Token Has Been Expired, Kindly Get A New Token To Continue Using This Bot.
          ㅤㅤㅤㅤㅤ   - Thank You 
 \nआपका विज्ञापन टोकन समाप्त हो गया है, बॉट को फिर से उपयोग करने के लिए नया टोकन लें!
          ㅤㅤㅤㅤㅤㅤㅤ- धन्यवाद
 \nValidity: {get_readable_time(VERIFY_EXPIRE)}
-\n#Verification...⌛</b>"""
+\n#Verification...⌛</blockquote></b>"""
     
     await message.reply_photo(
         photo=VERIFY_PHOTO,
         caption=text,
         reply_markup=buttons,
         reply_to_message_id=message.id,
-        protect_content=True,
     )
  
 async def get_verify_token(bot, userid, link):
@@ -79,16 +79,16 @@ async def get_verify_token(bot, userid, link):
     if not short_url:
         token = ''.join(random.choices(string.ascii_letters + string.digits, k=9))
         long_link = f"{link}verify-{userid}-{token}"
-        short_url = await short_url(long_link)
+        short_url = await get_short_url(long_link)
         vdict.update({'token': token, 'short_url': short_url})
     return short_url
 
-async def short_url(longurl, _shortener = SHORTLINK_URL, _shortener_api = SHORTLINK_API):
+async def get_short_url(longurl, shortener_site = SHORTLINK_SITE, shortener_api = SHORTLINK_API):
     cget = create_scraper().request
     disable_warnings()
     try:
-        url = f'https://{_shortener}/api'
-        params = {'api': _shortener_api,
+        url = f'https://{shortener_site}/api'
+        params = {'api': shortener_api,
                   'url': longurl,
                   'format': 'text',
                  }
@@ -102,14 +102,15 @@ async def short_url(longurl, _shortener = SHORTLINK_URL, _shortener_api = SHORTL
             if res.status_code == 200:
                 return res.get('shortenedUrl', long_url)
     except Exception as e:
+        print(e)
         return long_link
 
 async def validate_token(client, message, data):
     user_id = message.from_user.id
     vdict = verify_dict.setdefault(user_id, {})
     dict_token = vdict.get('token', None)
-    if is_user_verified(user_id):
-        return await message.reply("<b>Sɪʀ, Yᴏᴜ Aʀᴇ Aʟʀᴇᴀᴅʏ Vᴇʀɪғɪᴇᴅ 🤓...</b>")  
+    if await is_user_verified(user_id):
+        return await message.reply("<b>Sɪʀ, Yᴏᴜ Aʀᴇ Aʟʀᴇᴀᴅʏ Vᴇʀɪғɪᴇᴅ 🤓...</b>")
     if not dict_token:
         return await send_verification(client, message, text="<b>Tʜᴀᴛ's Nᴏᴛ Yᴏᴜʀ Vᴇʀɪғʏ Tᴏᴋᴇɴ 🥲...\n\n\nTᴀᴘ Oɴ Vᴇʀɪғʏ Tᴏ Gᴇɴᴇʀᴀᴛᴇ Yᴏᴜʀs</b>")  
     _, uid, token = data.split("-")
@@ -117,11 +118,13 @@ async def validate_token(client, message, data):
         return await send_verification(client, message, text="<b>Tʜᴀᴛ's Nᴏᴛ Yᴏᴜʀ Vᴇʀɪғʏ Tᴏᴋᴇɴ 🥲...\n\n\nTᴀᴘ Oɴ Vᴇʀɪғʏ Tᴏ Gᴇɴᴇʀᴀᴛᴇ Yᴏᴜʀs</b>")
     elif dict_token != token:
         return await send_verification(client, message, text="<b>Iɴᴠᴀʟɪᴅ Oʀ Exᴘɪʀᴇᴅ Tᴏᴋᴇɴ 🔗...</b>")
+    await verifydb.update_verify_status(user_id)
     await message.reply_photo(photo=VERIFY_PHOTO,
                               caption=f'<b>Wᴇʟᴄᴏᴍᴇ Bᴀᴄᴋ 😁, Nᴏᴡ Yᴏᴜ Cᴀɴ Usᴇ Mᴇ Fᴏʀ {get_readable_time(VERIFY_EXPIRE)}.\n\n\nEɴᴊᴏʏʏʏ...❤️</b>',
                               reply_to_message_id=message.id,
                              )
-    verify_dict.pop(user_id, None)
+    
+    vdict = {}
     
 def get_readable_time(seconds):
     periods = [('ᴅ', 86400), ('ʜ', 3600), ('ᴍ', 60), ('s', 1)]
@@ -132,7 +135,8 @@ def get_readable_time(seconds):
             result += f'{int(period_value)}{period_name}'
     return result
 
-
-# Usage 😀
-
+'''
+Note ⚠️: Add This Code In Root Of Your Repo Or Anywhere You Want.
+Credit : @GK-BOTZ
+'''
                 
